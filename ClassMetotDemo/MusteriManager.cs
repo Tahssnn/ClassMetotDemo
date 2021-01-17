8a4a1c2ad519c5be3b9cd6be1b9e10682c209508
Tahsin ŞENER <50183953+Tahssnn@users.noreply.github.com>
2021-01-18 00:08:16 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adında yeni bir müşteri eklendi!");
+        }
+
+        public void silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id +" numarasına sahip müşteri çıkarıldı!");
+        }
+
+        public void listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Cinsiyet + " Cinsiyeti olan müşteriler listelendi!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Tahsin";
+            musteri1.Soyad = "Şener";
+            musteri1.Cinsiyet = "Erkek";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Esma";
+            musteri2.Soyad = "Çalışkan";
+            musteri2.Cinsiyet = "Kadın";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Fatih";
+            musteri3.Soyad = "Korkmaz";
+            musteri3.Cinsiyet = "Erkek";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.Cinsiyet);
+                Console.WriteLine("*******************************");
+            }
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.ekle(musteri1);
+            musteriManager.silme(musteri2);
+            musteriManager.listele(musteri3);
+
+
+
+        }
+    }
+}
